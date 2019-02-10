984a0e8e360249ae7b7dd771033051c8b7ebffce
skpai27 <skpai27@yahoo.com.sg>
2019-02-10 23:37:18 +0800

    Added jUnit test file (FindByNumberCommandTest.java) for findNum command

--- test/java/seedu/addressbook/commands/FindByNumberCommandTest.java
+++ test/java/seedu/addressbook/commands/FindByNumberCommandTest.java
@@ -1,16 +1,50 @@
 package seedu.addressbook.commands;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.util.TypicalPersons;
 
 public class FindByNumberCommandTest {
 
-    @Test
-    public void getNumber() {
-    }
+    private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
+    private final TypicalPersons td = new TypicalPersons();
 
     @Test
-    public void execute() {
+    public void execute() throws IllegalValueException {
+        //same number, same case: matched
+        assertFindByNumberCommandBehavior("91119111", Arrays.asList(td.amy));
+
+        //different number, no match
+        assertFindByNumberCommandBehavior("13576", Collections.emptyList());
+
     }
+
+    /**
+     * Executes the find command for the given keywords and verifies
+     * the result matches the persons in the expectedPersonList exactly.
+     */
+    private void assertFindByNumberCommandBehavior(String number, List<ReadOnlyPerson> expectedPersonList) {
+        FindByNumberCommand command = createFindByNumberCommand(number);
+        CommandResult result = command.execute();
+
+        assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
+    }
+
+    private FindByNumberCommand createFindByNumberCommand(String number) {
+        FindByNumberCommand command = new FindByNumberCommand(number);
+        command.setData(addressBook, Collections.emptyList());
+        return command;
+    }
+
 }
