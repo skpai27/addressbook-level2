d17af1e9edba302c5c06a336f09a9ef07005477b
skpai27 <skpai27@yahoo.com.sg>
2019-02-10 19:58:24 +0800

    Modified user guide to include method findNum, added jUnit test file for findNum method, but test file contains nothing yet

---
+++ test/java/seedu/addressbook/commands/FindByNumberCommandTest.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.commands;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindByNumberCommandTest {
+
+    @Test
+    public void getNumber() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
